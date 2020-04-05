e5b9f40f9f9f9e028ec6f32f6f40fd0c5808c1bf
Luna McNulty <l.mcnulty@riseup.net>
2020-04-05 13:08:55 -0400

    Update stencil

--- trigger.py
+++ trigger.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import csv
-from random import randint
+from random import randint, random
 
 compatability = {}
 
@@ -53,99 +53,142 @@ def get_user_purchases(id):
 		reader = csv.reader(append)
 		return [row for row in reader if row[0] == id]
 
-
-loyalty_id = "43610000389"
-loyalty_id = "43610003420"
-
-
-# Selection all the products purchased by the subject in the daily download. (755)
-purchases = get_user_purchases(loyalty_id)
-
-ex("Get the subject purchases")
-out_list(purchases)
-
-
-# From this list, keep the products with 0 or 1 star 
-purchases = [row for row in purchases if get_stars(row[3]) < 2]
-
-ex("From this list, keep the products with 0 or 1 star")
-out_list(purchases)
-
-categories = set()
-
-# which has a brand(?) and a sub categories known.
-with open("data/CouponedUPCS.csv") as couponedUPCS:
-	reader = csv.reader(couponedUPCS)
-	categoried = []
-	for row in reader:
-		for purchase in purchases:
-			if row[0] == purchase[3]:
-				categories.add(row[-1])
-
-triggers = []
-coupons = []
-
-# Select the coupon from the same categories of these products.
-with open ("data/CouponCodebookSASIMPEDIT.csv") as f:
-	reader = csv.reader(f)
-	for row in reader:
-		if row[0] == "CouponNumber": continue
-		coupon = Coupon(row)
-		coupons.append(coupon)
-		if row[3] in categories:
-			triggers.append(Coupon(row))
-
-
-preferences = get_user_preferences(loyalty_id)
-
-triggers = [
-	coupon for coupon in triggers if coupon.is_compatible(preferences)
-]
-
-ex("Keep only the coupon which can be use by the subject")
-for coupon in triggers: print(coupon)
-
-triggers = coupons
-
+# This should return the GQPI for the category of the coupon for the
+# customer in `customer_id`
+def get_gqpi(customer_id, coupon):
+	return random()
+	
 def get_lift(loyalty_id, coupon1, coupon2):
 	return 1
 
-def get_improvement(loyalty_id, coupon):
-	return 1
+def select(loyalty_id):
 
-selection = []
+	# Selection all the products purchased by the subject in the daily download. (755)
+	purchases = get_user_purchases(loyalty_id)
 
-# If there is no trigger
-if len(triggers) < 1:
-	pass
-
-# If there is one trigger
-elif len(triggers) == 1:
-	pass
-
-# If there is two triggers
-elif len(triggers) == 2:
-	selection = triggers
-
-# If there is more than two triggers
-else:
-	ex("There are more than two triggers")
-
-	# give a weight to all the triggers according if the GQPI categories
-	# they improve need to be improve or not
-	triggers.sort(key=lambda x: get_improvement(loyalty_id, x))
-
-	# Select the first coupon using this weight
-	selection.append(triggers[-1])
-
-	# Give a weight to all the other triggers according to the lift between
-	# the first coupon and the trigger
-	triggers.sort(key=lambda x: get_lift(loyalty_id, selection[0], x)
-
-	# Select the second coupon using this weight
-	selection.append(triggers[-1])
+	ex("Get the subject purchases")
+	out_list(purchases)
 
 
+	# From this list, keep the products with 0 or 1 star 
+	purchases = [row for row in purchases if get_stars(row[3]) < 2]
+
+	ex("From this list, keep the products with 0 or 1 star")
+	out_list(purchases)
+
+	categories = set()
+
+	# which has a brand(?) and a sub categories known.
+	with open("data/CouponedUPCS.csv") as couponedUPCS:
+		reader = csv.reader(couponedUPCS)
+		categoried = []
+		for row in reader:
+			for purchase in purchases:
+				if row[0] == purchase[3]:
+					categories.add(row[-1])
+
+	triggers = []
+	coupons = []
+
+	preferences = get_user_preferences(loyalty_id)
+	
+	# Select the coupon from the same categories of these products.
+	with open ("data/CouponCodebookSASIMPEDIT.csv") as f:
+		reader = csv.reader(f)
+		for row in reader:
+			if row[0] == "CouponNumber": continue
+			coupon = Coupon(row)
+			if not coupon.is_compatible(preferences): continue
+			coupons.append(coupon)
+			if row[3] in categories:
+				triggers.append(Coupon(row))
+
+	ex("Keep only the coupon which can be use by the subject")
+	for coupon in triggers: print(coupon)
+
+	#triggers = coupons
 
 
 
+	selection = []
+
+	# If there is no trigger
+	if len(triggers) < 1:
+		# use the worst GQPI (or HEI if we don’t have the GQPI data 
+		# or if the subject had purchased for less than 50$ in the 
+		# month) categories with coupon that could been used by the 
+		# subject as a list of possible coupon. Choose the first coupon 
+		# randomly in this list.
+		coupons.sort(key=lambda x: get_gqpi(loyalty_id, x))
+		selection.append(coupons[-1])
+
+		# Add to the list of possible coupon the list of coupon that can 
+		# improve the next worst GQPI(or HEI) category.
+		possible_pairs = [
+			coupon for coupon in coupons 
+			if coupon.category != selection[0].category
+		]
+		# Give a weight to all the coupon using the lift between two
+		# coupon. 
+		possible_paris.sort(
+			key=lambda coupon: get_lift(loyalty_id, selection[0], coupon)
+		)
+
+		# Choose randomly (?) the second coupon using the weight.
+		selection.append(possible_pairs[-1])
+
+	# If there is one trigger
+	elif len(triggers) == 1:
+
+		# the first coupon is the trigger.
+		selection = triggers
+
+		# Select the list of coupon improving the worst 3 GQPI (or HEI)  
+		# categories. Give a weight to all the coupon using the lift  
+		# between two coupon. Choose randomly the second coupon using 
+		# the weight.
+		# (I don't really understand what this means, so I'm just using
+		#  the best lift score given the first selection.)
+		selection.push(
+			sorted(coupons, key=lambda coupon: get_lift(loyalty_id, selection[0], coupon))[-1]
+		)
+
+	# If there is two triggers
+	elif len(triggers) == 2:
+		# the two coupon will be the two triggers
+		selection = triggers
+
+	# If there is more than two triggers
+	else:
+		ex("There are more than two triggers")
+
+		# give a weight to all the triggers according if the GQPI 
+		# categories they improve need to be improve or not
+		triggers.sort(key=lambda x: get_gqpi(loyalty_id, x))
+
+		# Select the first coupon using this weight
+		selection.append(triggers[-1])
+
+		# Give a weight to all the other triggers according to the 
+		# lift between the first coupon and the trigger
+		triggers.sort(key=lambda x: get_lift(loyalty_id, selection[0], x))
+
+		# Select the second coupon using this weight
+		selection.append(triggers[-1])
+
+	# TODO:
+	# Select the educational content randomly in the list of educational 
+	# content that can be related with one of the two coupons and that 
+	# had not been send to the subject in the past weeks.
+
+	# TODO:
+	# If one of the coupon can be part of one of the recipes and that
+	# this recipes had not been send in the past weeks, select this 
+	# recipes.
+
+# This function gets called if you run the script directly, but not if
+# you import it as a library.
+if __name__ == "__main__":
+	loyalty_id = "43610000389"
+	loyalty_id = "43610003420"
+	select(loyalty_id)
